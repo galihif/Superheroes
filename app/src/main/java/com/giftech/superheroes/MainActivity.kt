9047c29909dbdc39e4c0acb733ddbee04df6f326
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-10-12 19:57:08 +0700

    create list heroes

--- app/src/main/java/com/giftech/superheroes/MainActivity.kt
+++ app/src/main/java/com/giftech/superheroes/MainActivity.kt
@@ -3,15 +3,33 @@ package com.giftech.superheroes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.animation.*
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.Spring.DampingRatioLowBouncy
+import androidx.compose.animation.core.Spring.StiffnessVeryLow
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.giftech.superheroes.data.HeroesRepository
+import com.giftech.superheroes.model.Hero
 import com.giftech.superheroes.ui.theme.SuperheroesTheme
 
+@ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +40,46 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    SuperheroesApp()
+                }
+            }
+        }
+    }
+}
+
+@ExperimentalAnimationApi
+@Composable
+fun SuperheroesApp() {
+    val visibleState = remember {
+        MutableTransitionState(false).apply {
+            // Start the animation immediately.
+            targetState = true
+        }
+    }
+    Scaffold() {
+        AnimatedVisibility(
+            visibleState = visibleState,
+            enter = fadeIn(
+                animationSpec = spring(dampingRatio = DampingRatioLowBouncy)
+            ),
+            exit = fadeOut()
+        ) {
+            LazyColumn(
+            ) {
+                itemsIndexed(HeroesRepository.heroes) { index, hero ->
+                    HeroItem(hero,
+                        Modifier
+                            .padding(horizontal = 16.dp, vertical = 8.dp)
+                            .animateEnterExit(
+                                enter = slideInVertically(
+                                    animationSpec = spring(
+                                        stiffness = StiffnessVeryLow,
+                                        dampingRatio = DampingRatioLowBouncy
+                                    ),
+                                    initialOffsetY = { it * (index + 1) } // staggered entrance
+                                )
+                            )
+                    )
                 }
             }
         }
@@ -30,14 +87,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HeroItem(hero: Hero, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier,
+        elevation = 2.dp
+    ) {
+        Row(
+            Modifier
+                .padding(16.dp)
+                .sizeIn(minHeight = 72.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(Modifier.weight(1f)) {
+                Text(
+                    stringResource(id = hero.nameRes),
+                    style = MaterialTheme.typography.h3
+                )
+                Text(
+                    stringResource(id = hero.descriptionRes),
+                    style = MaterialTheme.typography.body1
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .size(72.dp)
+                    .clip(RoundedCornerShape(8.dp))
+
+            ) {
+                Image(
+                    painter = painterResource(hero.imageRes),
+                    contentDescription = null,
+                    alignment = Alignment.TopCenter,
+                    contentScale = ContentScale.FillWidth
+                )
+            }
+        }
+    }
 }
 
+@ExperimentalAnimationApi
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SuperheroesTheme {
-        Greeting("Android")
+        SuperheroesApp()
     }
 }
